142fe427cc510bc4f4fbc13ef42425fa662a6bfd
Amph <amprubio@ucm.es>
2021-04-13 17:43:09 +0200

    Proyecto creado

---
+++ TrailsPrototype/Assets/Trails/DecitionWindow.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class DecitionWindow : EditorWindow
+{
+    List<Node>openNodes;
+    [MenuItem("Window/TrAIl/DecitionTree")]
+    public static void ShowWindow()
+    {
+        GetWindow<DecitionWindow>("DecitionTree");
+    }
+
+    //window code
+   void OnGUI()
+   {
+        GUILayout.Label("Decition", EditorStyles.boldLabel);
+
+        if(GUILayout.Button("Create Node"))
+        {
+            Debug.Log("Node created");
+        }
+
+   }
+
+    void AddNode()
+    {
+        openNodes.Add(new Node());
+    }
+
+
+}
+
+public class Node: MonoBehaviour
+{
+    
+    MonoBehaviour myAction;
+    public Node()
+    {
+
+    }
+    public Node(string mAction)
+    {
+
+    }
+
+}
