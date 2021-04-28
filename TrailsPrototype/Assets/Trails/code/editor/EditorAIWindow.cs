97a3b8d084ff95feb46124423f53daaea59e8a54
Amph <amprubio@ucm.es>
2021-04-28 17:47:41 +0200

    reinicio para una arquitectura mas decente

---
+++ TrailsPrototype/Assets/Trails/code/editor/EditorAIWindow.cs
@@ -0,0 +1,54 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+public class EditorAIWindow : EditorWindow
+{
+    private const string SaveDataPath = "Assets/Resources/TrailsSaveData.asset";
+
+
+
+
+
+    [MenuItem("Window/Trails")]
+    private static void ShowWindow()
+    {
+        EditorWindow window = GetWindow<EditorAIWindow>("Behaviour tree");
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+       
+    }
+
+    private void OnGUI()
+    {
+      
+    }
+
+    private void DrawSelectionGUI()
+    {
+  
+    }
+
+    private void SaveSelectedPreset(string name)
+    {
+       
+    }
+
+    private void RenameSelectedPreset(string name)
+    {
+       
+    }
+
+    private void DeleteSelectedPreset()
+    {
+    
+    }
+
+    private void DrawConfigGUI()
+    {
+       
+    }
+}
