53ac4f703cad8dba324f1ce21cbf73e7e81d7ad8
Amph <amprubio@ucm.es>
2021-05-13 16:20:54 +0200

    Editores

---
+++ TrailsPrototype/Assets/Trails/Source/Editor/Core/BTEditorCanvas.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.Events;
+
+namespace Trails.Editor{
+public class BTEditorCanvas 
+{
+    private const int MOUSE_DRAG=2;
+    public event UnityAction OnRepaint;
+
+    private int mSnapSize;
+    private static BTEditorCanvas mEditorCanvas_;
+
+        public Vector2 Position { get; set; }
+		public Rect Area { get; set; }
+		public bool IsDebuging { get; set; }
+		public string Clipboard { get; set; }
+
+        public BTEditorCanvas()
+		{
+			Position = Vector2.zero;
+			Area = new Rect();
+			IsDebuging = false;
+			mSnapSize = 10;
+			Clipboard = null;
+		}
+        public static BTEditorCanvas Current{
+            get{
+            return mEditorCanvas_;
+            }
+            set{
+            mEditorCanvas_=value;
+            }
+        }
+
+        public void Repaint(){
+            if(OnRepaint!=null)
+                OnRepaint();
+        }
+
+
+
+}
+}
---
+++ TrailsPrototype/Assets/Trails/Source/Editor/Core/BehaviourTreeEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Trails;
+using UnityEditor;
+
+namespace Trails.Editor{
+
+public class BehaviourTreeEditor : EditorWindow {
+   
+   [SerializeField]
+   private Texture mGrid_;
+   
+   [SerializeField]
+   private BTAsset mBTAsset_;
+
+    [SerializeField]
+   // private BTNavigationHistory mNavigationHistory_;
+
+
+        //private BTEditorGrid m_grid;
+		//private BTEditorGraph m_graph;
+		//private BTEditorCanvas m_canvas;
+
+
+private bool isDisposed_;
+
+     private void OnEnable() {
+        if(mGrid_==null){
+            mGrid_= Resources.Load<Texture>("Trails/EditorGUI/background");
+
+        }    
+
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+}
