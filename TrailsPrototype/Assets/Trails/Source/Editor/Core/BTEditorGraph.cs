f5e61caad94082472d0edb1391c6c1c3bd2b1814
Amph <amprubio@ucm.es>
2021-05-15 23:09:57 +0200

    Copyright de la serializacion de varias cosas

--- TrailsPrototype/Assets/Trails/Source/Editor/Core/BTEditorGraph.cs
+++ TrailsPrototype/Assets/Trails/Source/Editor/Core/BTEditorGraph.cs
@@ -1,18 +1,58 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
+
+using UnityEditor;
 using UnityEngine;
+using Trails;
 
-public class BTEditorGraph : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+
+namespace Trails.Editor{
+public class BTEditorGraph : ScriptableObject {
+    private const int SELECT_MOUSE_BUTTON = 0;
+	private const int CONTEXT_MOUSE_BUTTON = 1;
+    protected List<BTEditorGraphNode> mSelection_;
+    protected BTEditorGraphNode mMasterRoot_;
+    protected Stack<BTEditorGraphNode> mRootStack_;
+
+    protected bool mDrawSelectionBox_;
+	protected bool mIsBehaviourTreeReadOnly_;
+	protected bool mCanBeginBoxSelection_;
+	protected Vector2 mSelectionBoxStartPos_;
+    
+   public bool ReadOnly
+	{
+		get{
+			return mIsBehaviourTreeReadOnly || EditorApplication.isPlaying;
+		}
+	}
+
+	public int Depth{
+		get{
+			return Mathf.Max(mRootStack.Count - 1, 0);
+		}
+	}
+
+    public Rect? SelectionBox { get; set; }
+
+    protected BTEditorGraphNode WorkingRoot
+	{
+		get{
+			return mRootStack_.Peek();
+		}
+	}
+
+	protected void OnCreated(){
+			mMasterRoot_ = null;
+			mRootStack_ = new Stack<BTEditorGraphNode>();
+			mDrawSelectionBox_ = false;
+			mIsBehaviourTreeReadOnly_ = false;
+			mSelectionBoxStartPos_ = Vector2.zero;
+			SelectionBox = null;
+	}
+    protected void OnDestroy(){
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
+}
 }
