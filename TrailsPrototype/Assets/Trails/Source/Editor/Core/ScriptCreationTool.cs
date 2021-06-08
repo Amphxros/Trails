9ca825dfd889ca733825a6f4112e648a60295e8e
Amph <amprubio@ucm.es>
2021-06-08 16:14:09 +0200

    Ahora se deberian crear custom scripts con los comportamientos heredados

--- TrailsPrototype/Assets/Trails/Source/Editor/Core/ScriptCreationTool.cs
+++ TrailsPrototype/Assets/Trails/Source/Editor/Core/ScriptCreationTool.cs
@@ -13,40 +13,80 @@ namespace  TrailsEditor
                 string path = EditorUtility.SaveFilePanel("Save script", "", defaultFileName, "cs");
 			
                 if(!string.IsNullOrEmpty(path)){
-
+                    string customScript=System.IO.Path.GetFileNameWithoutExtension(path);
+                    using(var writer = System.IO.File.CreateText(path))
+    				{
+					writer.Write(CreateScriptContent(mScript, customScript));
+	    			}
+			    	AssetDatabase.Refresh();
                 }
             }
         
             private static string CreateScriptContent(CustomScriptType mScript, string scriptName){
 
-                return "";
+               string content = string.Empty;
+
+			try
+			{
+				TextAsset template = null;
+				switch(mScript)
+				{
+				case CustomScriptType.Composite:
+					template = Resources.Load<TextAsset>("Trails/Resources/Templates/Composite");
+					break;
+				case CustomScriptType.Decorator:
+					template = Resources.Load<TextAsset>("Trails/Resources/Templates/Decorator");
+					break;
+				case CustomScriptType.Action:
+					template = Resources.Load<TextAsset>("Trails/Resources/Templates/Action");
+					break;
+				case CustomScriptType.Constraint:
+					template = Resources.Load<TextAsset>("Trails/Resources/Templates/Constraint");
+					break;
+				case CustomScriptType.Service:
+					template = Resources.Load<TextAsset>("Trails/Resources/Templates/Service");
+					break;
+				}
+
+				if(template != null)
+					content = string.Format(template.text, scriptName);
+            }
+
+            catch (System.Exception e){
+                Debug.LogException(e);
+                content=string.Empty;
+            }
+
+            return content;
+            
             }
 
             [MenuItem("Assets/Create/Trails/Composite")]
             private static void CreateCustomCompositeScript()
 		    {
-			    
+			    CreateScript(CustomScriptType.Composite, "CustomComposite");
             }
 
             [MenuItem("Assets/Create/Trails/Decorator")]
             private static void CreateCustomDecoratorScript()
 		    {
 			    
+			    CreateScript(CustomScriptType.Decorator, "CustomDecorator");
             }
             [MenuItem("Assets/Create/Trails/Action")]
             private static void CreateCustomActionScript()
 		    {
-			    
+			    CreateScript(CustomScriptType.Action, "CustomAction");
             }
             [MenuItem("Assets/Create/Trails/Constraint")]
             private static void CreateCustomConstraintScript()
 		    {
-			    
+			    CreateScript(CustomScriptType.Constraint, "CustomConstrint");
             }
             [MenuItem("Assets/Create/Trails/Service")]
             private static void CreateCustomServiceScript()
 		    {
-			    
+			    CreateScript(CustomScriptType.Service, "CustomService");
             }
 
             
